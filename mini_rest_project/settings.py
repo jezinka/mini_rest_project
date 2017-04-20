b15787135b9aef6ca3fa5625d01e5a92091ae798
jezinka <jezina1989@gmail.com>
2017-04-20 13:11:18 +0200

    give up for now :P

--- mini_rest_project/settings.py
+++ mini_rest_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allow all host headers
-ALLOWED_HOSTS = ['young-beach-71108.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
