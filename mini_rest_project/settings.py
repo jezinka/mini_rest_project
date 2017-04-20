1d41be4e65267f9540b06c944c4f515e11206407
jezinka <jezina1989@gmail.com>
2017-04-20 13:09:20 +0200

    try to avoid 500 status on heroku

--- mini_rest_project/settings.py
+++ mini_rest_project/settings.py
@@ -23,11 +23,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['young-beach-71108.herokuapp.com']
 
 # Application definition
 
