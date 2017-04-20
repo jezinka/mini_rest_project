bb634c1c7c0cd99a96fd318381d2a93ad53a4e0b
jezinka <jezina1989@gmail.com>
2017-04-20 12:52:16 +0200

    try to avoid 400 status on heroku

--- mini_rest_project/settings.py
+++ mini_rest_project/settings.py
@@ -23,7 +23,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
